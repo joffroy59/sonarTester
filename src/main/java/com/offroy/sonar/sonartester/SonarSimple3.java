ef11d8b57b7236fb8af77b02d057eb1a2d0956ce
joffroy59 <jerome.offroy@gmail.com>
2015-04-26 15:24:36 +0200

    add some file to increase technical debt in sonar

---
+++ src/main/java/com/offroy/sonar/sonartester/SonarSimple3.java
@@ -0,0 +1,17 @@
+package com.offroy.sonar.sonartester;
+
+public class SonarSimple3 {
+    public String test1(String param1) {
+        if (true) System.out.println("true");
+        System.out.println("arf"+"1");
+        return "fdsdfsdf" + param1;
+
+    }
+
+    public String test2(String param1) {
+        if (true) System.out.println("true");
+        System.out.println("arf"+"1");
+        return "fdsdfsdf" + param1;
+
+    }
+}
---
+++ src/main/java/com/offroy/sonar/sonartester3/SonarSimple.java
@@ -0,0 +1,17 @@
+package com.offroy.sonar.sonartester3;
+
+public class SonarSimple {
+    public String test1(String param1) {
+        if (true) System.out.println("true");
+        System.out.println("arf"+"1");
+        return "fdsdfsdf" + param1;
+
+    }
+
+    public String test2(String param1) {
+        if (true) System.out.println("true");
+        System.out.println("arf"+"1");
+        return "fdsdfsdf" + param1;
+
+    }
+}
